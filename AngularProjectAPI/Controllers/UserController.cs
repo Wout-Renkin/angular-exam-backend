e1f4e3c21d27cae42c6c632351f261d9124d0130
mcloots <michael.cloots@gmail.com>
2020-09-04 16:16:07 +0200

    JWT Security

---
+++ AngularProjectAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularProjectAPI.Models;
+using AngularProjectAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+    }
+}
--- AngularProjectAPI/Models/Role.cs
+++ AngularProjectAPI/Models/Role.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AngularProjectAPI.Models
@@ -13,6 +14,7 @@ namespace AngularProjectAPI.Models
         public string Name { get; set; }
 
         //Relations
+        [JsonIgnore]
         public ICollection<User> Users { get; set; }
     }
 }
